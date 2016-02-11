5454344e737c33a1e5ac809f08882fdb267baf9d
Greg <gdaaronson@gmail.com>
2016-02-11 13:10:10 -0800

    Added unweighted graph, but it is not tested

--- src/DijkstraTest.java
+++ src/DijkstraTest.java
@@ -3,15 +3,13 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class DijkstraTest {
 
-	private Dijkstra d;
 	
-	@Before
-	public void setUp() throws Exception {
+	@Test
+	public void testA() {
 		@SuppressWarnings("unchecked")
 		Map<Integer, Double>[] neighbors = new Map[5];
 		for(int i = 0; i < neighbors.length; i++){
@@ -23,12 +21,28 @@ public class DijkstraTest {
 		neighbors[3].put(0, 2.0);
 		neighbors[4].put(2, 3.0);
 		Graph g = new Graph(neighbors);
-		d = new Dijkstra(3, 2, g);
+		Dijkstra d = new Dijkstra(3, 2, g);
+		assertEquals("[3, 0, 4, 2]", d.getPath().toString());
 	}
 
 	@Test
-	public void testRun() {
-		assertEquals("[3, 0, 4, 2]", d.getPath().toString());
+	public void testB() {
+		@SuppressWarnings("unchecked")
+		Map<Integer, Double>[] neighbors = new Map[5];
+		for(int i = 0; i < neighbors.length; i++){
+			neighbors[i] = new HashMap<Integer, Double>();
+		}
+		neighbors[0].put(1, 56.0);
+		neighbors[0].put(3, 8.0);
+		neighbors[1].put(0, 56.0);
+		neighbors[1].put(3, 9.0);
+		neighbors[2].put(1, 6.0);
+		neighbors[2].put(4, 12.0);
+		neighbors[3].put(2, 18.0);
+		neighbors[4].put(3, 20.0);
+		Graph g = new Graph(neighbors);
+		Dijkstra d = new Dijkstra(3, 0, g);
+		assertEquals("[3, 2, 1, 0]", d.getPath().toString());
 	}
 
 
