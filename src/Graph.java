8bc8411d14f070e410778f3ff5d1840c73a8795c
Greg <gdaaronson@gmail.com>
2016-03-01 13:33:01 -0800

    Expirement now works

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Graph {
@@ -7,7 +8,37 @@ public class Graph {
 	 * between neighbors
 	 */
 	private Map<Integer, Double>[] neighbors;
+	
+	public void expandGraph(int key, Edge gate){
+		int size = neighbors.length;
+		Map<Integer, Double>[] big= new HashMap [2*size];
+		for(int i = 0; i < size; i++){
+			big[i] = new HashMap<Integer, Double>();
+			big[i + size] = new HashMap<Integer, Double>();
+			for(int j : neighbors[i].keySet()){
+				big[i].put(j, neighbors[i].get(j));
+				big[i + size].put(j + size, neighbors[i].get(j));
+			}
+		}
+		neighbors = big;
+		addNeighbor(new Edge(gate.getStart() + size, gate.getEnd() + size, gate.getLength()));
+		addNeighbor(new Edge(key, key + size, 0));
+	}
 
+	public void addNeighbor(Edge requirement) {
+		neighbors[requirement.getStart()].put(requirement.getEnd(), requirement.getLength()); 
+	}
+	
+	@Override
+	public String toString() {
+		String s = "";
+		for(int i = 0; i < neighbors.length; i++){
+			s += i + " -> " + neighbors[i].keySet();
+			s += "\n";
+		}
+		return s;
+	}
+	
 	/**
 	 * The constructor which sets the neighbors
 	 * 
@@ -38,9 +69,25 @@ public class Graph {
 		this.neighbors = neighbors;
 	}
 
+	
 	/** The number of vertices that a graph has */
 	public int size() {
 		return neighbors.length;
+	}
+	
+	public Map<Integer, Double>[] getNeighbors() {
+		return neighbors;
+	}
+
+	public void remove(Map<Integer, Double> neighbors) {
+		neighbors.remove(neighbors.keySet());
+	}
+
+	public void remove(Map<Integer, Integer>[] requirement) {
+		for(int i = 0; i < neighbors.length; i++){
+			for(int j = 0; j < requirement.length; j++){
+			}
+		}
 	}
 
 }
