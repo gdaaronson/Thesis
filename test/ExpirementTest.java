b5bae0dc700443f6aa869b6545fde5cd5a2088ef
Gregory Aaronson <gregoryaaronson@MBPDEV18.local>
2018-03-05 11:59:32 -0800

    moved tests to a different directory for intelij

--- test/ExpirementTest.java
+++ test/ExpirementTest.java
@@ -1,10 +1,11 @@
-import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 
 public class ExpirementTest {
 
